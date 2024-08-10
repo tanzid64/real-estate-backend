e45725448dc619cd49124ecc7fe9e9de94d6da1a
tanzid64 <tanzid@inbox.ru>
2024-08-10 15:56:16 +0600

    feat: add timestaped, profile models, signals

--- config/settings/base.py
+++ config/settings/base.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from os import getenv, path
 
 from dotenv import load_dotenv
+import cloudinary
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
@@ -171,3 +172,14 @@ CELERY_TASK_SOFT_TIME_LIMIT = 60
 CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
 
 CELERY_WORKER_SEND_TASK_EVENTS = True
+
+# Cloudinary Settings
+CLOUDINARY_CLOUD_NAME = getenv('CLOUDINARY_CLOUD_NAME')
+CLOUDINARY_API_KEY = getenv('CLOUDINARY_API_KEY')
+CLOUDINARY_API_SECRET = getenv('CLOUDINARY_API_SECRET')
+
+cloudinary.config(
+    cloud_name=CLOUDINARY_CLOUD_NAME,
+    api_key=CLOUDINARY_API_KEY,
+    api_secret=CLOUDINARY_API_SECRET
+)
--- core_apps/profiles/admin.py
+++ core_apps/profiles/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from .models import Profile
 # Register your models here.
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+  list_display = (
+    "id",
+    "user",
+    "gender",
+    "occupation",
+    "slug"
+  )
+  list_display_links = (
+    "id",
+    "user",
+    "slug"
+  )
+  list_filter = ("occupation", "gender")
